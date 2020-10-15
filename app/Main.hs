10d3d82189f1ac46a5557d116c52933c7973c27f
Iarek Kovtunenko <16271112+iarekk@users.noreply.github.com>
2020-10-15 21:46:43 +0100

    Read JSON and write back
    run with echo 'data/tree-like.json' | stack run

--- app/Main.hs
+++ app/Main.hs
@@ -2,7 +2,19 @@
 
 module Main(main) where
 
+import Data.Aeson
+import Data.Aeson.Types
+import qualified Data.ByteString.Lazy.Char8 as BS
+
 import Lib
+import Types
 
 main :: IO ()
-main = someFunc
+main = do
+    s <- getLine
+    Prelude.putStrLn s
+    t <- BS.readFile s
+    -- BS.putStrLn t
+    let scope = Data.Aeson.decode t :: Maybe Scope
+    -- Prelude.putStrLn (show scope)
+    BS.putStrLn (Data.Aeson.encode scope)
