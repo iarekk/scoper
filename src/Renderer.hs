36ad2fa1a85f9c15ff1b1883e1d62940a1a83bec
Iarek Kovtunenko <iarek.kovtunenko@gmail.com>
2020-11-01 22:22:30 +0000

    Coloured outputs

--- src/Renderer.hs
+++ src/Renderer.hs
@@ -2,14 +2,26 @@
 
 module Renderer(renderScope) where
 import Graphics.Blank
+import qualified Graphics.Blank.Style as GBS
 import Types ( RenderableScope(RenderableScope) )
 import qualified Data.Text as T
+import qualified Data.Colour.SRGB as DCS
 
 renderScope :: RenderableScope -> Canvas()
-renderScope (RenderableScope n s e c h t) = do
-    rect(fromIntegral s*100, fromIntegral t*100, fromIntegral (e-s)*100, fromIntegral h*100)
-    font "20pt Calibri"
-    fillText(T.pack n, fromIntegral s*100, fromIntegral t*100)
+renderScope (RenderableScope n s e c h t col) = do
+    let x = fromIntegral s * 100 + 100
+    let y = fromIntegral t * 50 + 100
+    let w = fromIntegral (e-s) * 100
+    let he = fromIntegral h * 50
+
+    beginPath()
+    rect(x, y, w , he)
+    GBS.fillStyle $ ((DCS.sRGB24read $ col) :: DCS.Colour Double)
+    fill()
     stroke()
-    mapM renderScope c
-    stroke()    
+    font "20pt Calibri"
+    fillStyle "black"
+    fillText(T.pack n, x, y)
+    stroke()
+    mapM (renderScope) c
+    stroke()
