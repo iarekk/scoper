2c9a3d200741b52a1445af25b9cde966ce3dffa5
Iarek Kovtunenko <16271112+iarekk@users.noreply.github.com>
2020-11-01 15:44:27 +0000

    Create converter (#6)
    * converter module
    
    * pre-render with top and height set to 0
    
    * n-tree experimentation
    
    * Tops assigned
    
    * add padding of +1

---
+++ src/Converter.hs
@@ -0,0 +1,34 @@
+module Converter (preRender) where
+
+import Types
+
+preRender :: Scope -> RenderableScope
+preRender (Scope n s e c) = assignTops withHeight
+    where
+        withHeight = RenderableScope n s e newChildren height top
+        newChildren = mapList $ fmap preRender <$> c
+        top = 0 :: ScopeTop
+        height = getChildHeights c
+        mapList Nothing = []
+        mapList (Just xs) = xs
+
+assignTops :: RenderableScope -> RenderableScope
+assignTops rScope = newScope
+    where
+        RenderableScope n s e c h t = rScope
+        newScope = RenderableScope n s e newChildren h t
+        newChildren = assignTops' t c
+
+assignTops' :: ScopeTop -> RenderableScopeChildren -> RenderableScopeChildren
+assignTops' _ [] = []
+assignTops' prevTop (x:xs) = x':(assignTops' newTop xs)
+    where
+        RenderableScope n s e c h _ = x
+        x' = RenderableScope n s e (assignTops' (prevTop+1) c) h (prevTop+1)
+        newTop = prevTop + h
+
+getChildHeights :: ScopeChildren -> ScopeHeight
+getChildHeights Nothing = 1
+getChildHeights (Just xs) = 1 + (sum $ map getHeight xs)
+    where
+        getHeight (Scope _ _ _ c) = getChildHeights c
