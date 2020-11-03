ffad31b32156e2974e83b99517f507f3d7abd71a
Iarek Kovtunenko <iarek.kovtunenko@gmail.com>
2020-11-03 21:57:50 +0000

    text over rectangles

--- src/Renderer.hs
+++ src/Renderer.hs
@@ -9,19 +9,21 @@ import qualified Data.Colour.SRGB as DCS
 
 renderScope :: RenderableScope -> Canvas()
 renderScope (RenderableScope n s e c h t col) = do
-    let x = fromIntegral s * 100 + 100
-    let y = fromIntegral t * 50 + 100
-    let w = fromIntegral (e-s) * 100
-    let he = fromIntegral h * 50
+    let rowHeight = 50
+    let millisecondToPxRatio = 100
+    let topMargin = 20
+    let leftMargin = 20
+    let x = fromIntegral s * millisecondToPxRatio + leftMargin
+    let y = fromIntegral t * rowHeight + topMargin
+    let w = fromIntegral (e-s) * millisecondToPxRatio
+    let he = fromIntegral h * rowHeight
 
     beginPath()
     rect(x, y, w , he)
     GBS.fillStyle $ ((DCS.sRGB24read $ col) :: DCS.Colour Double)
     fill()
-    stroke()
+    mapM (renderScope) c
     font "20pt Calibri"
     fillStyle "black"
-    fillText(T.pack n, x, y)
-    stroke()
-    mapM (renderScope) c
-    stroke()
+    textBaseline middle
+    fillText(T.pack n, x + 5, y + rowHeight/2)
