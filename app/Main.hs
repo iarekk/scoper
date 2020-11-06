ed986c064082b3a3de9f3693574f3edc179e8c57
Iarek Kovtunenko <iarek.kovtunenko@gmail.com>
2020-11-06 23:40:01 +0000

    add toTree

--- app/Main.hs
+++ app/Main.hs
@@ -1,9 +1,9 @@
 module Main(main) where
 
 import Graphics.Blank 
-import Converter(preRender)
+import Converter(preRender, toTree)
 import Parsers(runParser)
-import Storage(readScope, writeRenderableScope)
+import Storage(readScope)
 import Renderer(renderScope)
 
 main :: IO ()
@@ -11,7 +11,8 @@ main = do
     runOptions <- runParser
     scope <- readScope runOptions
     let renScope = preRender scope
-    writeRenderableScope $ renScope
+    print renScope
+    print $ toTree scope
     blankCanvas 3000 $ \ context -> do -- start blank canvas on port 3000
             send context $ do                 -- send commands to this specific context
                     renderScope renScope
--- src/Converter.hs
+++ src/Converter.hs
@@ -1,4 +1,4 @@
-module Converter (preRender) where
+module Converter (preRender, toTree) where
 
 import Types
 
@@ -33,7 +33,7 @@ assignTops rScope = newScope
 
 assignTops' :: ScopeTop -> RenderableScopeChildren -> RenderableScopeChildren
 assignTops' _ [] = []
-assignTops' prevTop (x:xs) = x':(assignTops' newTop xs)
+assignTops' prevTop (x:xs) = x': assignTops' newTop xs
     where
         RenderableScope n s e c h _ _ = x
         x' = RenderableScope n s e (assignTops' (prevTop+1) c) h (prevTop+1) ""
@@ -41,10 +41,15 @@ assignTops' prevTop (x:xs) = x':(assignTops' newTop xs)
 
 getChildHeights :: ScopeChildren -> ScopeHeight
 getChildHeights Nothing = 1
-getChildHeights (Just xs) = 1 + (sum $ map getHeight xs)
+getChildHeights (Just xs) = 1 + sum (map getHeight xs)
     where
         getHeight (Scope _ _ _ c) = getChildHeights c
 
      
 chartColours :: [String]
 chartColours = cycle ["#3366cc","#dc3912","#ff9900","#109618","#990099","#0099c6","#dd4477","#66aa00","#b82e2e","#316395","#3366cc","#994499","#22aa99","#aaaa11","#6633cc","#e67300","#8b0707","#651067","#329262","#5574a6","#3b3eac","#b77322","#16d620","#b91383","#f4359e","#9c5935","#a9c413","#2a778d","#668d1c","#bea413","#0c5922","#743411"]
+
+toTree :: Scope -> ScopeTree
+toTree (Scope n s e c) = N (ScopeData n s e) (mp c) where
+    mp Nothing = []
+    mp (Just xs) = map toTree xs
