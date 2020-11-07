116edcc63ad3194bd755a59095ea956042ec8f60
Iarek Kovtunenko <iarek.kovtunenko@gmail.com>
2020-11-07 01:30:23 +0000

    clean-up

--- app/Main.hs
+++ app/Main.hs
@@ -1,7 +1,7 @@
 module Main(main) where
 
 import Graphics.Blank 
-import Converter(preRender, toTree)
+import Converter(preRender)
 import Parsers(runParser)
 import Storage(readScope)
 import Renderer(renderScope)
@@ -11,9 +11,8 @@ main = do
     runOptions <- runParser
     scope <- readScope runOptions
     let renScope = preRender scope
-    print renScope
-    print $ toTree scope
-    blankCanvas 3000 $ \ context -> do -- start blank canvas on port 3000
-            send context $ do                 -- send commands to this specific context
+    putStrLn "open http://localhost:3000 to see the diagram"
+    blankCanvas 3000 $ \ context -> -- start blank canvas on port 3000
+            send context $                  -- send commands to this specific context
                     renderScope renScope
 
--- src/Converter.hs
+++ src/Converter.hs
@@ -1,6 +1,6 @@
-module Converter (preRender, toTree, getHeight, getTops, getColours) where
+module Converter (preRender) where
 
-import qualified Data.Traversable as DT
+import Data.Traversable(mapAccumL)
 import           Types
 
 preRender :: Scope -> RenderableScope
@@ -10,13 +10,6 @@ preRender scope = fmap toRender treeWithColours
         treeWithHeights = getHeight scopeTree
         treeWithTops = getTops treeWithHeights 0
         treeWithColours = getColours treeWithTops chartColours
-
-        --(Scope n s e c) = scope
-        -- h = 0
-        -- t = 0
-        -- col = "#ff00ff"
-        -- rc = []
-
 
 mapList :: Maybe [a] -> [a]
 mapList Nothing   = []
@@ -33,14 +26,13 @@ addChildHeights (t:ts) = h + addChildHeights ts where
     N (_,h) _ = t
 
 getTops :: NT (ScopeData, ScopeHeight) -> ScopeTop -> NT (ScopeData, ScopeHeight, ScopeTop)
-getTops t st = snd $ DT.mapAccumL f st t where
+getTops t st = snd $ mapAccumL f st t where
     f top (sd, h) = (top+1, (sd, h, top))
 
 getColours :: NT (ScopeData, ScopeHeight, ScopeTop) -> [ScopeColour] -> NT (ScopeData, ScopeHeight, ScopeTop, ScopeColour)
-getColours t cols = snd $ DT.mapAccumL f cols t where
+getColours t cols = snd $ mapAccumL f cols t where
     f (c:cs) (sd, h, top) = (cs, (sd, h, top, c))
     f [] _ = error "colours are meant to be infinite"
-
 
 chartColours :: [ScopeColour]
 chartColours = cycle ["#3366cc","#dc3912","#ff9900","#109618","#990099","#0099c6","#dd4477","#66aa00","#b82e2e","#316395","#994499","#22aa99","#aaaa11","#6633cc","#e67300","#8b0707","#651067","#329262","#5574a6","#3b3eac","#b77322","#16d620","#b91383","#f4359e","#9c5935","#a9c413","#2a778d","#668d1c","#bea413","#0c5922","#743411"]
