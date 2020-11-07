945e8abed593c4581c669dd96a540058b7412952
Iarek Kovtunenko <16271112+iarekk@users.noreply.github.com>
2020-11-07 01:02:25 +0000

    just fix heights

--- src/Converter.hs
+++ src/Converter.hs
@@ -1,55 +1,91 @@
-module Converter (preRender, toTree) where
+module Converter (preRender, toTree, getHeight, getTops, getColours) where
 
-import Types
+import qualified Data.Traversable as DT
+import           Types
 
 preRender :: Scope -> RenderableScope
-preRender (Scope n s e c) = assignColours (assignTops withHeight) chartColours
+preRender scope = fmap toRender treeWithColours
     where
-        withHeight = RenderableScope n s e newChildren height top ""
-        newChildren = mapList $ fmap preRender <$> c
-        top = 0 :: ScopeTop
-        height = getChildHeights c
-        mapList Nothing = []
-        mapList (Just xs) = xs
+        scopeTree = toTree scope
+        treeWithHeights = getHeight scopeTree
+        treeWithTops = getTops treeWithHeights
+        treeWithColours = getColours treeWithTops chartColours
 
-assignColours :: RenderableScope -> [RenderableScopeColour] -> RenderableScope
-assignColours (RenderableScope n s e c h t _) (col:cs) = RenderableScope n s e newChildren h t col
-    where
-        newChildren = colourTheChildren c cs
+        --(Scope n s e c) = scope
+        -- h = 0
+        -- t = 0
+        -- col = "#ff00ff"
+        -- rc = []
 
-colourTheChildren :: RenderableScopeChildren -> [RenderableScopeColour] -> RenderableScopeChildren
-colourTheChildren [] _ = []
-colourTheChildren (ch:chs) (col:cls) = c':(colourTheChildren chs cls)
-    where
-        RenderableScope n s e c h t _ = ch
-        c' = RenderableScope n s e (colourTheChildren c cls) h t col 
 
-assignTops :: RenderableScope -> RenderableScope
-assignTops rScope = newScope
-    where
-        RenderableScope n s e c h t _ = rScope
-        newScope = RenderableScope n s e newChildren h t ""
-        newChildren = assignTops' t c
+mapList :: Maybe [a] -> [a]
+mapList Nothing   = []
+mapList (Just xs) = xs
 
-assignTops' :: ScopeTop -> RenderableScopeChildren -> RenderableScopeChildren
-assignTops' _ [] = []
-assignTops' prevTop (x:xs) = x': assignTops' newTop xs
-    where
-        RenderableScope n s e c h _ _ = x
-        x' = RenderableScope n s e (assignTops' (prevTop+1) c) h (prevTop+1) ""
-        newTop = prevTop + h
+getHeight :: NT ScopeData -> NT (ScopeData, ScopeHeight)
+getHeight (N sd ts) = N (sd, chs) nts where
+    nts = map getHeight ts
+    chs = addChildHeights nts
 
-getChildHeights :: ScopeChildren -> ScopeHeight
-getChildHeights Nothing = 1
-getChildHeights (Just xs) = 1 + sum (map getHeight xs)
-    where
-        getHeight (Scope _ _ _ c) = getChildHeights c
+addChildHeights :: [NT (ScopeData, ScopeHeight)] -> ScopeHeight
+addChildHeights [] = 1
+addChildHeights (t:ts) = h + addChildHeights ts where
+    N (_,h) _ = t
 
-     
-chartColours :: [String]
+getTops :: NT (ScopeData, ScopeHeight) -> NT (ScopeData, ScopeHeight, ScopeTop)
+getTops t = snd $ DT.mapAccumL f 0 t where
+    f top (sd, h) = (top+h, (sd, h, top))
+
+getColours :: NT (ScopeData, ScopeHeight, ScopeTop) -> [ScopeColour] -> NT (ScopeData, ScopeHeight, ScopeTop, ScopeColour)
+getColours t cols = snd $ DT.mapAccumL f cols t where
+    f (c:cs) (sd, h, top) = (cs, (sd, h, top, c))
+
+
+
+--getHeight t ts 
+    -- map getHeight where
+    -- getHeight (N t c) = N (t, 0)
+    -- --getHeight (N _ xs) = undefined
+
+-- assignColours :: RenderableScope -> [RenderableScopeColour] -> RenderableScope
+-- assignColours (RenderableScope n s e c h t _) (col:cs) = RenderableScope n s e newChildren h t col
+--     where
+--         newChildren = colourTheChildren c cs
+
+-- colourTheChildren :: RenderableScopeChildren -> [RenderableScopeColour] -> RenderableScopeChildren
+-- colourTheChildren [] _ = []
+-- colourTheChildren (ch:chs) (col:cls) = c':(colourTheChildren chs cls)
+--     where
+--         RenderableScope n s e c h t _ = ch
+--         c' = RenderableScope n s e (colourTheChildren c cls) h t col
+
+-- assignTops :: RenderableScope -> RenderableScope
+-- assignTops rScope = newScope
+--     where
+--         RenderableScope n s e c h t _ = rScope
+--         newScope = RenderableScope n s e newChildren h t ""
+--         newChildren = assignTops' t c
+
+-- assignTops' :: ScopeTop -> RenderableScopeChildren -> RenderableScopeChildren
+-- assignTops' _ [] = []
+-- assignTops' prevTop (x:xs) = x': assignTops' newTop xs
+--     where
+--         RenderableScope n s e c h _ _ = x
+--         x' = RenderableScope n s e (assignTops' (prevTop+1) c) h (prevTop+1) ""
+--         newTop = prevTop + h
+
+-- getChildHeights :: ScopeChildren -> ScopeHeight
+-- getChildHeights Nothing = 1
+-- getChildHeights (Just xs) = 1 + sum (map getHeight xs)
+--     where
+--         getHeight (Scope _ _ _ c) = getChildHeights c
+
+
+chartColours :: [ScopeColour]
 chartColours = cycle ["#3366cc","#dc3912","#ff9900","#109618","#990099","#0099c6","#dd4477","#66aa00","#b82e2e","#316395","#3366cc","#994499","#22aa99","#aaaa11","#6633cc","#e67300","#8b0707","#651067","#329262","#5574a6","#3b3eac","#b77322","#16d620","#b91383","#f4359e","#9c5935","#a9c413","#2a778d","#668d1c","#bea413","#0c5922","#743411"]
 
-toTree :: Scope -> ScopeTree
-toTree (Scope n s e c) = N (ScopeData n s e) (mp c) where
-    mp Nothing = []
-    mp (Just xs) = map toTree xs
+toTree :: Scope -> NT ScopeData
+toTree (Scope n s e c) = N (ScopeData n s e) (map toTree (mapList c))
+
+toRender :: (ScopeData, ScopeHeight, ScopeTop, ScopeColour) -> RenderableScopeData
+toRender (sd, h, t, c) = RenderableScopeData sd h t c
