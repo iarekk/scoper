59c4e96d04224a510d48ef09596c8fc0bdbe1e44
Iarek Kovtunenko <16271112+iarekk@users.noreply.github.com>
2020-11-08 22:33:53 +0000

    code cleanup

--- src/Converter.hs
+++ src/Converter.hs
@@ -10,10 +10,6 @@ preRender scopeTree = fmap toRender treeWithColours
         treeWithTops = getTops treeWithHeights 0
         treeWithColours = getColours treeWithTops chartColours
 
-mapList :: Maybe [a] -> [a]
-mapList Nothing   = []
-mapList (Just xs) = xs
-
 getHeight :: NT ScopeData -> NT (ScopeData, ScopeHeight)
 getHeight (N sd ts) = N (sd, chs) nts where
     nts = map getHeight ts
@@ -31,9 +27,6 @@ getColours t cols = snd $ mapAccumL f cols t where
 
 chartColours :: [ScopeColour]
 chartColours = cycle ["#3366cc","#dc3912","#ff9900","#109618","#990099","#0099c6","#dd4477","#66aa00","#b82e2e","#316395","#994499","#22aa99","#aaaa11","#6633cc","#e67300","#8b0707","#651067","#329262","#5574a6","#3b3eac","#b77322","#16d620","#b91383","#f4359e","#9c5935","#a9c413","#2a778d","#668d1c","#bea413","#0c5922","#743411"]
-
-toTree :: Scope -> NT ScopeData
-toTree (Scope n s e c) = N (ScopeData n s e) (map toTree (mapList c))
 
 toRender :: (ScopeData, ScopeHeight, ScopeTop, ScopeColour) -> RenderableScopeData
 toRender (sd, h, t, c) = RenderableScopeData sd h t c
--- src/Types.hs
+++ src/Types.hs
@@ -21,7 +21,6 @@ data NT a = N a [NT a]
 
 type MillisecondsToPxRatio = Double
 type RenderableScope       = NT RenderableScopeData
-type ScopeChildren         = Maybe [Scope]
 type ScopeColour           = String
 type ScopeEnd              = Int
 type ScopeHeight           = Int
@@ -31,16 +30,6 @@ type ScopeTop              = Int
 type ScopeTree             = NT ScopeData
 
 data DrawingMetadata = DrawingMetadata MillisecondsToPxRatio
-
-data Scope = Scope
-    { name :: ScopeName
-    , s    :: ScopeStart
-    , e    :: ScopeEnd
-    , c    :: ScopeChildren
-    } deriving (Generic, Show)
-instance ToJSON Scope
-instance FromJSON Scope
-
 data RenderableScopeData = RenderableScopeData
     ScopeData
     ScopeHeight
