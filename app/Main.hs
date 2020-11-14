9a42284ae584c4e68e7234b672cca1943bbd169f
Iarek Kovtunenko <16271112+iarekk@users.noreply.github.com>
2020-11-14 23:31:44 +0000

    Read the actual SLA doc

--- app/Main.hs
+++ app/Main.hs
@@ -4,12 +4,13 @@ import           Converter      (preRender)
 import           Graphics.Blank
 import           Parsers        (runParser)
 import           Renderer       (drawDiagram)
-import           Storage        (readScope)
+import           Storage        (readScope, showScope)
 
 main :: IO ()
 main = do
     runOptions <- runParser
     scope <- readScope runOptions
+    showScope scope
     let renScope = preRender scope
     putStrLn "open http://localhost:3000 to see the diagram"
     blankCanvas 3000 $ \ context -> -- start blank canvas on port 3000
